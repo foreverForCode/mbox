63772476c0d168771838cc14fc4751ca7a948052
jorden <www.freewm@163.com>
2017-11-02 18:13:32 +0800

    修改

--- vue-插件/plugin/vueMbox/mbox.js
+++ vue-插件/plugin/vueMbox/mbox.js
@@ -1,113 +1,275 @@
 /**
  * Updated by linxin on 2017/7/27.
  */
-var Toast = {};
-var showToast = false, // 存储toast显示状态
-    showLoad = false, // 存储loading显示状态
-    toastVM = null, // 存储toast vm
-    loadNode = null; // 存储loading节点元素
 
 Toast.install = function (Vue, options) {
 
-    var opt = {
-        defaultType: 'bottom',
-        duration: '2500',
-        wordWrap: false
+    // 公共变量 和 公共 方法
+    var doc = document,
+        JM = {
+            timer: {},
+            endCall: {}
+        },
+        regPxe = /\px|pt|em|rem/g,
+        $QM = function (elem, root) {
+            return doc.querySelectorAll(root ? (root + " " + elem) : elem);
+        }; // $QM ----------------> 获取节点
+
+    JM.isType = function (obj, type) {
+        type = type.replace(/(\w)/, function (v) {
+            return v.toUpperCase()
+        });
+        return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+    }; //  判断对象的类型
+    //事件点击
+    JM.onTouch = function (elem, callback) {
+        var move, touchStartY;
+        if (!/Android|iPhone|SymbianOS|Windows Phone|iPad|iPod|ios/.test(navigator.userAgent)) {
+            return elem.addEventListener("click", function (e) {
+                callback.call(this, e);
+            }, false);
+        } else {
+            elem.addEventListener("touchmove", function (e) {
+                touchStartY = e.changedTouches[0].clientY;
+                move = true;
+            }, false);
+            elem.addEventListener("touchend", function (e) {
+                if (Math.abs(e.changedTouches[0].clientY - touchStartY) > 10) return;
+                e.preventDefault();
+                move || callback.call(this, e);
+                move = false;
+            }, false);
+        }
     };
-    for (var property in options) {
-        opt[property] = options[property];
-    }
-
-
-    // $toast功能
-    Vue.prototype.$toast = function (tips, type) {
-        console.log('tips: '+tips,'type: '+type);
-        var curType = type ? type : opt.defaultType;
-        var wordWrap = opt.wordWrap ? 'lx-word-wrap' : '';  // lx-word-wrap
-        var domWidth = opt.width ? 'style="width: ' + opt.width + '"' : '';
-        // var tmp = '<div v-show="show" :class="[type,typess]" class="lx-toast ' + wordWrap + '" ' + domWidth + '><div class="title"></div>{{tip}}</div>';
-        var tmp = `
-            <div v-show="show" :class="[type,typess]" :style = "domWidth">
-                <div class="title"></div>
-                <div class="content">{{tip}}</div>
-                <div class="footer">
-                    <button>确定</button>
-                    <button>取消</button>
-                </div>
-            </div>
-        `;
-        if (showToast) {
-            // 如果toast还在，则不再执行
+    JM.toCssArr = function (attr) {
+        return Object.keys(attr).map(function (k) {
+            return k + ":" + attr[k]
+        });
+    }; // 遍历css的属性
+    JM.goCss = function (elem, attr) {
+        var val, cssNumber = {
+                'column-count': 1,
+                'columns': 1,
+                'box-flex': 1,
+                'line-clamp': 1,
+                'font-weight': 1,
+                'opacity': 1,
+                'z-index': 1,
+                'zoom': 1
+            },
+            toLower = function (str) {
+                return str.replace(/::/g, '/').replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2').replace(/([a-z\d])([A-Z])/g, '$1_$2').replace(/_/g, '-').toLowerCase()
+            },
+            addPx = function (name, value) {
+                return (typeof value == "number" && !cssNumber[toLower(name)]) ? value + "px" : value
+            };
+        if (JM.isType(attr, "object")) {
+            for (var key in attr) {
+                elem.style[toLower(key)] = addPx(key, attr[key]);
+            }
+        } else {
+            val = document.defaultView.getComputedStyle(elem, null)[attr];
+            return regPxe.test(val.toString()) ? Math.round(val.toString().replace(regPxe, "")) : val;
+        }
+    };
+    var idx = 1, jemCell = ["jembox"],
+    JemBox = function (options) {
+        //默认配置
+        var config = {
+            cell: "",
+            width: "",  // 弹层的宽度，可用是百分比，可用是如（320px）
+            height: "",  // 弹层的高度，可用是百分比，可用是如（320px）
+            radius: "4px",  //弹层的类型
+            title: [],  // 标题可以有2个参数，例如 ["标题",{"background-color":"#fff","color":"#fff"}]，如果title:[]参数为空，不显示标题
+            content: "请稍等,暂无内容！",  // 内容	
+            conStyle: {}, //内容框的css样式，你可以写更多样式，例如{"background-color":"#fff"}
+            skin: "scale",  //弹出风格
+            yesCall: null,  // 确定按钮回调函数，例如{name:"确定",css:{"background-color":"#fff"},fun:function (){}}
+            noCall: null,  // 取消按钮回调函数，例如{name:"取消",css:{"background-color":"#fff"},fun:function (){}}
+            closeCall: null,  // 右上角关闭取消按钮回调函数
+            closeBtn: [false, 1],  // 参数一是否显示右上角关闭取消按钮，默认false，参数二按钮颜色 1是黑色，2是白色
+            time: null,  // 自动关闭时间(毫秒)
+            mask: true,  //是否显示遮罩层
+            maskClose: true,  // 点击遮罩层是否关闭，默认true
+            maskColor: "rgba(0,0,0,0.5)",  // 遮罩层颜色可以是rgba也可以是rgb
+            padding: "10px 10px",
+            zIndex: 1e4,  // 层级关系
+            success: null,  //层成功弹出层的回调函数，返回一个参数为当前层元素对象
+            endCall: null   //层彻底销毁后的回调函数
+        };
+        var that = this, newobj = JSON.parse(JSON.stringify(config));
+        that.opts = Object.assign(newobj, options);
+        var Wcell = that.opts.content;
+        if (Wcell && Wcell.nodeType === 1) {
+            JM.WisShow = JM.goCss(Wcell, "display");
+            JM.Wprev = Wcell.previousSibling;
+            JM.Wnext = Wcell.nextSibling;
+            JM.Wparent = Wcell.parentNode;
+        }
+        that.viewInit();
+    };
+var jefn = Vue.prototype =JemBox.prototype;
+jefn.viewInit = function () {
+    var that = this, opts = that.opts,
+        modDiv = doc.createElement("div"),
+        maskDiv = doc.createElement("div"),
+        cellidx = (opts.cell == "" || opts.cell == null) ? idx : opts.cell;
+    //判断是否有标题参数，创建标题	
+    var titDiv = function () {
+        var titName, tCss;
+        if (JM.isType(opts.title, "array") && opts.title.length > 0) {
+            titName = opts.title[0];
+            tCss = JM.isType(opts.title[1], "object") ? JM.toCssArr(opts.title[1]).join(";") : "";
+        } else if (opts.title != "") {
+            titName = opts.title;
+        } else {
+            tCss = "height:0px;";
+        }
+        return '<div class="jemboxhead" id="head' + cellidx + '" style="' + (tCss || "") + '">' + (titName || "") + "</div>";
+    }();
+    //判断是否有按钮参数，创建按钮
+    var btnDiv = function () {
+        var yesArr = opts.yesCall, noArr = opts.noCall, footCss, btnDom;
+        //判断按钮对象是否为空
+        if (yesArr == null && noArr == null) {
+            footCss = "display:none;height:0px;";
+            btnDom = "";
+        } else {
+            if ((yesArr && yesArr.name) && (noArr && noArr.name)) {
+                //确认与取消都不为空的情况
+                var yesCss = JM.isType(yesArr.css, "object") ? JM.toCssArr(yesArr.css).join(";") : "",
+                    noCss = JM.isType(noArr.css, "object") ? JM.toCssArr(noArr.css).join(";") : "";
+                btnDom = '<span type="no" style="' + noCss + '">' + noArr.name + '</span><span type="yes" style="' + yesCss + '">' + yesArr.name + "</span>";
+            } else {
+                //确认与取消部分为空的情况
+                if ((yesArr && yesArr.name) && noArr == null) {
+                    var btnCss = JM.isType(yesArr.css, "object") ? JM.toCssArr(yesArr.css).join(";") : "";
+                    btnDom = '<span type="yes" style="' + btnCss + '">' + yesArr.name + "</span>";
+                } else if (yesArr == null && (noArr && noArr.name)) {
+                    var btnCss = JM.isType(noArr.css, "object") ? JM.toCssArr(noArr.css).join(";") : "";
+                    btnDom = '<span type="no" style="' + btnCss + '">' + noArr.name + "</span>";
+                }
+            }
+        }
+        return '<div class="jemboxfoot" id="foot' + cellidx + '" style="' + (footCss || "") + '">' + btnDom + "</div>";
+    }();
+    that.id = modDiv.id = jemCell[0] + cellidx;
+    modDiv.setAttribute("class", "jemboxer jemboxmain-" + opts.skin);
+    modDiv.setAttribute("jmcellidx", cellidx);
+    modDiv.setAttribute("skin", opts.skin);
+    JM.goCss(modDiv, { "pointer-events": "none", "z-index": opts.zIndex });
+    //设置遮罩层
+    maskDiv.setAttribute("id", "jemmask" + cellidx);
+    maskDiv.classList.add("jemboxmask");
+    JM.goCss(maskDiv, { "background-color": opts.maskColor, "z-index": opts.zIndex - 5 });
+    //弹层模板元素
+    modDiv.innerHTML =
+        '<div class="jemboxchild ' + opts.skin + '">' + titDiv +
+        '<span class="jemboxclose0' + opts.closeBtn[1] + '" style="display:' + (opts.closeBtn[0] ? "block" : "none") + '"></span>' +
+        '<div class="jemboxmcont" style="padding:' + opts.padding + ';"></div>' + btnDiv +
+        '</div>';
+    //把弹层插入到body内之下
+    if (opts.mask) doc.body.appendChild(maskDiv);
+    doc.body.appendChild(modDiv);
+    var elem = that.elem = $QM("#" + that.id)[0],
+        setWH = { "width": opts.width != "" ? opts.width : "", "height": opts.height != "" ? opts.height : "" },
+        conStyle = Object.assign({ "pointer-events": "auto", "background-color": "#fff", "border-radius": opts.radius }, opts.conStyle);
+    JM.goCss($QM("#" + that.id + " .jemboxchild")[0], Object.assign(setWH, conStyle));
+    that.idx = (opts.cell == "" || opts.cell == null) ? idx++ : opts.cell;
+    that.contype(opts);
+    that.action(opts);
+    setTimeout(function () {
+        try {  //弹出时添加动画效果
+            $QM(".jemboxchild", "#" + that.id)[0].classList.add("jemanim-" + opts.skin);
+        } catch (e) {
             return;
         }
-        if (!toastVM) {
-            var toastTpl = Vue.extend({
-                data: function () {
-                    return {
-                        show: showToast,
-                        tip: tips,
-                        type: 'lx-toast-' + curType,
-                        domWidth:domWidth,
-                        typess:'lx-toast ' + wordWrap
-                    }
-                },
-                template: tmp
-            });
-            toastVM = new toastTpl()
-            var tpl = toastVM.$mount().$el;
-            document.body.appendChild(tpl);
-        }
-        toastVM.type = 'lx-toast-' + curType;
-        toastVM.tip = tips;
-        toastVM.show = showToast = true;
-
-        setTimeout(function () {
-            toastVM.show = showToast = false;
-        }, opt.duration)
+        //弹出成功后的回调
+        opts.success && opts.success(elem);
+    }, 5);
+};
+jefn.contype = function (opts) {
+    //设置显示的类型
+    var that = this, ctype, tid = "#" + that.id, msg = opts.content, conCell = $QM(".jemboxmcont", tid)[0];
+    if (msg === undefined) return conCell;
+    if (typeof msg === "string") {
+        conCell.innerHTML = opts.content;
+        ctype = "string";
+    } else if (msg && msg.nodeType === 1) {
+        conCell.innerHTML = "";
+        var wrap = doc.createElement("div");
+        if (JM.goCss(msg, "display") == "none") JM.goCss(msg, { "display": "block" });
+        conCell.appendChild(wrap.appendChild(msg));
+        ctype = "html";
     };
-    ['bottom', 'center', 'top'].forEach(function (type) {
-        Vue.prototype.$toast[type] = function (tips) {
-            return Vue.prototype.$toast(tips, type)
-        }
-    });
-
-
-    // $loading功能
-    Vue.prototype.$loading = function (tips, type) {
-        if (type == 'close') {
-            loadNode.show = showLoad = false;
-        } else {
-            if (showLoad) {
-                // 如果loading还在，则不再执行
-                return;
-            }
-            var loadTpl = Vue.extend({
-                data: function () {
-                    return {
-                        show: showLoad
-                    }
-                },
-                template: '<div v-show="show" class="lx-load-mark"><div class="lx-load-box"><div class="lx-loading"><div class="loading_leaf loading_leaf_0"></div><div class="loading_leaf loading_leaf_1"></div><div class="loading_leaf loading_leaf_2"></div><div class="loading_leaf loading_leaf_3"></div><div class="loading_leaf loading_leaf_4"></div><div class="loading_leaf loading_leaf_5"></div><div class="loading_leaf loading_leaf_6"></div><div class="loading_leaf loading_leaf_7"></div><div class="loading_leaf loading_leaf_8"></div><div class="loading_leaf loading_leaf_9"></div><div class="loading_leaf loading_leaf_10"></div><div class="loading_leaf loading_leaf_11"></div></div><div class="lx-load-content">' + tips + '</div></div></div>'
+    $QM(tid)[0].setAttribute("contype", ctype);
+};
+jefn.action = function (opts) {
+    var that = this, tid = "#" + that.id, yesArr = opts.yesCall, noArr = opts.noCall;
+    //自动关闭
+    if (opts.time) {
+        JM.timer[that.idx] = setTimeout(function () {
+            mBox.close(that.idx);
+        }, opts.time * 1e3);
+    }
+    //关闭按钮
+    if (opts.closeBtn[0]) {
+        var elclose = $QM(".jemboxclose0" + opts.closeBtn[1], tid)[0];
+        JM.onTouch(elclose, function () {
+            //关闭按钮回调函数
+            opts.closeCall && opts.closeCall();
+            mBox.close(that.idx);
+        });
+    }
+    //确认取消
+    if (yesArr || noArr) {
+        var btnArr = $QM(".jemboxfoot", tid)[0].children;
+        for (var i = 0; i < btnArr.length; i++) {
+            JM.onTouch(btnArr[i], function () {
+                var onytpe = this.getAttribute("type");
+                if (onytpe == "yes") {
+                    //确定按钮回调函数
+                    if (JM.isType(yesArr.fun, "function")) yesArr.fun && yesArr.fun(that.idx);
+                } else {
+                    //取消按钮回调函数
+                    if (JM.isType(noArr.fun, "function")) noArr.fun && noArr.fun();
+                    mBox.close(that.idx);
+                }
             });
-            loadNode = new loadTpl();
-            var tpl = loadNode.$mount().$el;
-
-            document.body.appendChild(tpl);
-            loadNode.show = showLoad = true;
         }
-    };
+    }
+    //点遮罩关闭
+    if (opts.mask && opts.maskClose) {
+        var oncover = $QM("#jemmask" + that.idx)[0];
+        JM.onTouch(oncover, function () {
+            mBox.close(that.idx, opts.endCall);
+        });
+    }
+    opts.endCall && (JM.endCall[that.idx] = opts.endCall);
+};
+// 让传入的元素在对话框关闭后可以返回到原来的地方
+function backSitu(elem, prev, next, parent, isShow) {
+    JM.goCss(elem.firstChild, { "display": isShow == "block" ? "block" : "none" });
+    if (prev && prev.parentNode) {
+        prev.parentNode.insertBefore(elem.firstChild, prev.nextSibling);
+    } else if (next && next.parentNode) {
+        next.parentNode.insertBefore(elem.firstChild, next);
+    } else if (parent) {
+        parent.appendChild(elem.firstChild);
+    }
+    this.backSitu = null;
+};
+//核心方法
+var mBox = function (options) {
+    var newm = new JemBox(options || {});
+    return newm.idx;
+};
 
-    ['open', 'close'].forEach(function (type) {
-        Vue.prototype.$loading[type] = function (tips) {
-            return Vue.prototype.$loading(tips, type)
-        }
-    });
+//获取元素节点
+mBox.Q = function (elem) {
+    return $QM(elem)[0];
+};
+
+
+
 }
-
-// if(typeof exports == 'object'){
-//     module.exports = Toast
-// }else if(typeof define == 'function' && define.amd){
-//     define([],function(){return Toast})
-// }else if(window.Vue){
-//     Vue.use(Toast)
-// }
